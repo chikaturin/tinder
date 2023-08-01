679ba52ba4fa4f163b48b39a4dfbfb69b1ba4c92
chikaturin <ninhhoaithanh@gmail.com>
2023-08-01 09:59:34 +0700

    met

--- animation.js
+++ animation.js
@@ -1,6 +1,9 @@
-function start()
-{
-    var a=parseInt(document.getElementById('txtA').value);
-    var b=parseInt(document.getElementById('txtB').value);
-    alert(a+b);
-}
+const openmenu=document.getElementById('open')
+const closemenu=document.getElementById('close')
+openmenu.addEventListener("click", function() {
+    if (closemenu.style.display === "none") {
+      closemenu.style.display = "block";
+    } else {
+      closemenu.style.display = "none";
+    }
+  });
